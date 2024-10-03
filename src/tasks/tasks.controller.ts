4a193770410ade67c39f24d51e6e5da21585c0ba
Irwan Hermawan <rwnhermawan@gmail.com>
2024-10-03 19:51:09 +0700

    initial module, service, controller

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {
+  }
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTasks(createTaskDto);
+  }
+}
